2a1ce9bff57b1e4511f3343abadcbe0914b50ac2
truongvu1508 <ntv150804@gmail.com>
2025-04-16 22:37:49 +0700

    add component AboutTarget

---
+++ client/src/components/client/AboutTarget/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AboutTarget.scss";
+
+const AboutTarget = () => {
+  return (
+    <div>
+      <h1>AboutTarget</h1>
+    </div>
+  );
+};
+
+export default AboutTarget;
