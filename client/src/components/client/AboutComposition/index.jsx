6d373b38fa0636376a76ccd0c009fa2bae391d94
truongvu1508 <ntv150804@gmail.com>
2025-04-16 23:43:09 +0700

    update

--- client/src/components/client/AboutComposition/index.jsx
+++ client/src/components/client/AboutComposition/index.jsx
@@ -1,11 +1,65 @@
 import React from "react";
 import "./AboutComposition.scss";
+import { assets } from "../../../assets/assets";
+import { Button, Col, Row } from "antd";
+import { useClerk, useUser } from "@clerk/clerk-react";
 
 const AboutComposition = () => {
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
   return (
-    <div>
-      <h1>AboutComposition</h1>
-    </div>
+    <section className="aboutComposition">
+      <div className="container" style={{ padding: "20px" }}>
+        <Row justify="center" align="middle">
+          <Col
+            xs={24}
+            sm={24}
+            md={24}
+            lg={12}
+            xl={12}
+            style={{ padding: "10px" }}
+          >
+            <h3 className="aboutComposition__title">
+              Học tập mọi lúc, mọi nơi
+            </h3>
+            <p className="aboutComposition__text">Học tập mọi lúc, mọi nơi</p>
+            {user ? (
+              <></>
+            ) : (
+              <>
+                <Button
+                  onClick={openSignIn}
+                  className="aboutComposition__button"
+                >
+                  Đăng Ký Tài Khoản
+                </Button>
+              </>
+            )}
+          </Col>
+          <Col
+            xs={24}
+            sm={24}
+            md={24}
+            lg={12}
+            xl={12}
+            style={{ padding: "10px" }}
+          >
+            <div className="aboutComposition__card">
+              <div className="aboutComposition__card-icon">
+                <img src={assets.icon_learn} alt="" />
+              </div>
+              <h4 className="aboutComposition__card-title">
+                Xây dựng cộng đồng
+              </h4>
+              <p className="aboutComposition__card-text">
+                B2HTV với mong muốn giúp đỡ các bạn sinh viên, lập trình viên
+                mới bước vào ngành IT trở thành một lập trình viên giỏi
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </section>
   );
 };
 
