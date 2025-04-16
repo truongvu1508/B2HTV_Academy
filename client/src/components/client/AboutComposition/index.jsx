1b530c14eca82018686c64b8008af3abeaed328e
truongvu1508 <ntv150804@gmail.com>
2025-04-16 23:34:19 +0700

    add component AboutComposition

---
+++ client/src/components/client/AboutComposition/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AboutComposition.scss";
+
+const AboutComposition = () => {
+  return (
+    <div>
+      <h1>AboutComposition</h1>
+    </div>
+  );
+};
+
+export default AboutComposition;
