12ead34048faded67a19676e1459b5062793731c
truongvu1508 <ntv150804@gmail.com>
2025-04-19 23:16:46 +0700

    update

--- client/src/pages/student/Player/index.jsx
+++ client/src/pages/student/Player/index.jsx
@@ -104,6 +104,9 @@ const Player = () => {
                 </div>
               ))}
           </div>
+          <div className="flex items-center gap-2 py-3 mt-10">
+            <h1 className="text-xl font-bold">Rate this Course:</h1>
+          </div>
         </div>
         {/* right column */}
         <div className="md:mt-10">
