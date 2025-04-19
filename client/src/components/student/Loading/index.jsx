d12f8e4ae4979f0a953b1e7d4885893d02b2daf9
truongvu1508 <ntv150804@gmail.com>
2025-04-19 16:38:50 +0700

    add loading animation

--- client/src/components/student/Loading/index.jsx
+++ client/src/components/student/Loading/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading</h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-10 sm:w-15 aspect-square border-4 border-gray-300 border-t-4 border-t-dark-1 rounded-full animate-spin"></div>
     </div>
   );
 };
