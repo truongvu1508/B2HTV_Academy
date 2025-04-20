cb63adc09cea3ad439d6e7a9dbb07e9370ae5336
truongvu1508 <ntv150804@gmail.com>
2025-04-20 22:53:11 +0700

    add page MyCourses

--- client/src/pages/educator/MyCourses/index.jsx
+++ client/src/pages/educator/MyCourses/index.jsx
@@ -18,6 +18,51 @@ const MyCourses = () => {
     <div className="h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0">
       <div className="w-full">
         <h2 className="pb-4 text-lg font-medium"> Danh sách khóa học</h2>
+        <div>
+          <table className="md:table-auto table-fixed w-full overflow-hidden border border-gray-500/20 border-collapse mt-10">
+            <thead className="text-gray-900 border-b border-gray-500/20 text-sm text-left">
+              <tr>
+                <th className="px-4 py-3 font-semibold truncate">
+                  All Courses
+                </th>
+                <th className="px-4 py-3 font-semibold truncate">Earnings</th>
+                <th className="px-4 py-3 font-semibold truncate">Students</th>
+                <th className="px-4 py-3 font-semibold truncate">
+                  Published On
+                </th>
+              </tr>
+            </thead>
+            <tbody className="text-sm text-gray-500">
+              {courses.map((course) => (
+                <tr key={course.id} className="border-b border-gray-500/20">
+                  <td className="md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3 truncate">
+                    <img
+                      src={course.courseThumbnail}
+                      alt="Course Image"
+                      className="w-16"
+                    />
+                    <span className="truncate hidden md:block">
+                      {course.courseTitle}
+                    </span>
+                  </td>
+                  <td className="px-4 py-3">
+                    {Math.floor(
+                      course.enrolledStudents.length * course.coursePrice -
+                        (course.discount * course.coursePrice) / 100
+                    )}{" "}
+                    {currency}
+                  </td>
+                  <td className="px-4 py-3">
+                    {course.enrolledStudents.length}
+                  </td>
+                  <td className="px-4 py-3">
+                    {new Date(course.createdAt).toLocaleDateString()}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   ) : (
