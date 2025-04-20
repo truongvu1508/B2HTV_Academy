d683bdbf2c2b79a8aed2dc96a67d3ca392983ade
truongvu1508 <ntv150804@gmail.com>
2025-04-20 22:41:00 +0700

    add page Dashboard

--- client/src/pages/educator/Dashboard/index.jsx
+++ client/src/pages/educator/Dashboard/index.jsx
@@ -1,10 +1,64 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../../context/AppContext";
+import { dummyDashboardData } from "../../../assets/assets";
+import Loading from "../../../components/student/Loading";
+import { FaBook, FaMoneyCheckAlt, FaUser } from "react-icons/fa";
+import "./Dashboard.scss";
 
 const Dashboard = () => {
-  return (
-    <div className="min-h-screen">
-      <h1>Educator Dashboard</h1>
+  const { currency } = useContext(AppContext);
+  const [dashboardData, setDashboardData] = useState(null);
+
+  const fetchDashboardData = async () => {
+    setDashboardData(dummyDashboardData);
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+  }, []);
+
+  return dashboardData ? (
+    <div className="min-h-screen flex flex-col items-start justify-between gap-8 md:p-8 md:pb-0 p-4 pt-8 pb-0">
+      <div className="space-y-5">
+        <div className="flex flex-wrap gap-5 items-center">
+          <div className="flex items-center gap-3 shadow-card border border-blue-500 p-4 w-56 rounded-md">
+            <div className="flex items-center">
+              <FaUser className="text-dark-2 text-3xl mr-[10px]" />
+              <div>
+                <p className="text-2xl font-medium text-gray-600">
+                  {dashboardData.enrolledStudentsData.length}
+                </p>
+                <p className="text-base text-gray-500"> Học viên</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center gap-3 shadow-card border border-blue-500 p-4 w-56 rounded-md">
+            <div className="flex items-center">
+              <FaBook className="text-dark-2 text-3xl mr-[10px]" />
+              <div>
+                <p className="text-2xl font-medium text-gray-600">
+                  {dashboardData.totalCourses}
+                </p>
+                <p className="text-base text-gray-500"> Khóa học</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center gap-3 shadow-card border border-blue-500 p-4 w-56 rounded-md">
+            <div className="flex items-center">
+              <FaMoneyCheckAlt className="text-dark-2 text-3xl mr-[10px]" />
+              <div>
+                <p className="text-2xl font-medium text-gray-600">
+                  {dashboardData.totalEarnings} {currency}
+                </p>
+                <p className="text-base text-gray-500"> Doanh thu</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  ) : (
+    <Loading />
   );
 };
 
--- client/src/pages/educator/MyCourses/index.jsx
+++ client/src/pages/educator/MyCourses/index.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../../context/AppContext";
+import Loading from "../../../components/student/Loading";
 
 const MyCourses = () => {
-  return (
-    <div className="min-h-screen">
-      <h1>My Courses Page</h1>
+  const { currency, allCourses } = useContext(AppContext);
+  const [courses, setCourses] = useState(null);
+
+  const fetchEducatorCourses = async () => {
+    setCourses(allCourses);
+  };
+
+  useEffect(() => {
+    fetchEducatorCourses();
+  }, []);
+
+  return courses ? (
+    <div className="h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0">
+      <div className="w-full">
+        <h2 className="pb-4 text-lg font-medium"> Danh sách khóa học</h2>
+      </div>
     </div>
+  ) : (
+    <Loading />
   );
 };
 
