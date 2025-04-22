83c8458b8be2b375876f2f1e3477f74282909636
truongvu1508 <ntv150804@gmail.com>
2025-04-22 17:48:34 +0700

    add popup AddLecture

--- client/src/pages/educator/AddCourse/index.jsx
+++ client/src/pages/educator/AddCourse/index.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from "react";
 import uniqid from "uniqid";
 import Quill from "quill";
 import { FiUpload } from "react-icons/fi";
+import { RiArrowDropDownLine, RiCloseFill } from "react-icons/ri";
+import { FaPlus } from "react-icons/fa";
 
 const AddCourse = () => {
   const quillRef = useRef(null);
@@ -21,6 +23,51 @@ const AddCourse = () => {
     lectureUrl: "",
     isPreviewFree: false,
   });
+
+  const handleChapter = (action, chapterId) => {
+    if (action === "add") {
+      const title = prompt("Enter Chapter Name:");
+      if (title) {
+        const newChapter = {
+          chapterId: uniqid(),
+          chapterTitle: title,
+          chapterContent: [],
+          collapsed: false,
+          chapterOrder:
+            chapters.length > 0 ? chapters.slice(-1)[0].chapterOrder + 1 : 1,
+        };
+        setChapters([...chapters, newChapter]);
+      }
+    } else if (action === "remove") {
+      setChapters(
+        chapters.filter((chapter) => chapter.chapterId !== chapterId)
+      );
+    } else if (action === "toggle") {
+      setChapters(
+        chapters.map((chapter) =>
+          chapter.chapterId === chapterId
+            ? { ...chapter, collapsed: !chapter.collapsed }
+            : chapter
+        )
+      );
+    }
+  };
+
+  const handleLecture = (action, chapterId, lectureIndex) => {
+    if (action === "add") {
+      setCurrentChapterId(chapterId);
+      setShowPopup(true);
+    } else if (action === "remove") {
+      setChapters(
+        chapters.map((chapter) => {
+          if (chapter.chapterId === chapterId) {
+            chapter.chapterContent.splice(lectureIndex, 1);
+          }
+          return chapter;
+        })
+      );
+    }
+  };
 
   useEffect(() => {
     //Initiate Quill only once
@@ -92,7 +139,159 @@ const AddCourse = () => {
               className="outline-none md:py-2.5 py-2 w-28 px-3 rounded border border-gray-500"
             />
           </div>
+
+          {/* Adding Chapters & Lectures */}
+          <div>
+            {chapters.map((chapter, chapterIndex) => (
+              <div
+                key={chapterIndex}
+                className="bg-white border rounded-lg mb-4"
+              >
+                <div className="flex justify-between items-center p-4 border-b">
+                  <div className="flex items-center">
+                    <RiArrowDropDownLine
+                      onClick={() => handleChapter("toggle", chapter.chapterId)}
+                      width={14}
+                      className={`mr-2 cursor-pointer transition-all ${
+                        chapter.collapsed && "-rotate-90"
+                      }`}
+                    />
+                    <span className="font-semibold">
+                      {chapterIndex + 1} {chapter.chapterTitle}
+                    </span>
+                  </div>
+                  <span className="text-gray-500">
+                    {chapter.chapterContent.length} Lectures
+                  </span>
+                  <RiCloseFill
+                    className="cursor-pointer"
+                    onClick={() => handleChapter("remove", chapter.chapterId)}
+                  />
+                </div>
+                {!chapter.collapsed && (
+                  <div className="p-4">
+                    {chapter.chapterContent.map((lecture, lectureIndex) => (
+                      <div
+                        key={lectureIndex}
+                        className="flex justify-between items-center mb-2"
+                      >
+                        <span>
+                          {lectureIndex + 1} {lecture.lectureTitle} -{" "}
+                          {lecture.lectureDuration} mins -{" "}
+                          <a
+                            href={lecture.lectureUrl}
+                            target="_blank"
+                            className="text-blue-500"
+                          >
+                            Link
+                          </a>{" "}
+                          - {lecture.isPreviewFree ? "Free Preview" : "Paid"}
+                        </span>
+                        <RiCloseFill
+                          onClick={() =>
+                            handleLecture("remove", chapter.chapterId)
+                          }
+                          className="cursor-pointer"
+                        />
+                      </div>
+                    ))}
+                    <div
+                      className="inline-flex bg-gray-100 p-2 rounded cursor-pointer mt-2"
+                      onClick={() => handleLecture("add", chapter.chapterId)}
+                    >
+                      + Add Lecture
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+          <div
+            className=" flex justify-center items-center bg-blue-100 p-2 rounded-lg cursor-pointer"
+            onClick={() => handleChapter("add")}
+          >
+            + Add Chapter
+          </div>
+          {showPopup && (
+            <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+              <div className="bg-white text-gray-700 p-4 rounded relative w-full max-w-80">
+                <h2 className="text-lg font-semibold mb-4">Add Lecture</h2>
+                <div className=" mb-2">
+                  <p>Lecture Title</p>
+                  <input
+                    type="text"
+                    className="mt-1 block w-full border rounded py-1 px-2"
+                    value={lectureDetails.lectureTitle}
+                    onChange={(e) =>
+                      setLectureDetails({
+                        ...lectureDetails,
+                        lectureTitle: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <div className=" mb-2">
+                  <p>Duration (minutes)</p>
+                  <input
+                    type="number"
+                    className="mt-1 block w-full border rounded py-1 px-2"
+                    value={lectureDetails.lectureDuration}
+                    onChange={(e) =>
+                      setLectureDetails({
+                        ...lectureDetails,
+                        lectureDuration: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <div className=" mb-2">
+                  <p>Lecture URL</p>
+                  <input
+                    type="text"
+                    className="mt-1 block w-full border rounded py-1 px-2"
+                    value={lectureDetails.lectureUrl}
+                    onChange={(e) =>
+                      setLectureDetails({
+                        ...lectureDetails,
+                        lectureUrl: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <div className=" flex gap-2 my-4">
+                  <p>Is Preview Free?</p>
+                  <input
+                    type="checkbox"
+                    className="mt-1 scale-125"
+                    value={lectureDetails.isPreviewFree}
+                    onChange={(e) =>
+                      setLectureDetails({
+                        ...lectureDetails,
+                        isPreviewFree: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <button
+                  type="button"
+                  className="w-full bg-blue-400 text-white px-4 py-2 rounded"
+                >
+                  Add
+                </button>
+                <RiCloseFill
+                  onClick={() => setShowPopup(false)}
+                  className="absolute top-2 right-2 text-[22px] cursor-pointer"
+                />
+              </div>
+            </div>
+          )}
         </div>
+        <button
+          type="submit"
+          className="bg-black text-white w-max py-2.5 px-8 rounded my-4"
+        >
+          ADD
+        </button>
       </form>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.get("/", (req, res) => res.send("API Working"));
 
 //Port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
