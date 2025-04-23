c092455372005f97212e06bf773d9c3268119506
truongvu1508 <ntv150804@gmail.com>
2025-04-23 14:30:27 +0700

    add Model User and Webhooks

---
+++ server/controllers/webhooks.js
@@ -0,0 +1,9 @@
+import { Webhook } from "svix";
+import User from "../models/User";
+
+// API Controller Function to Manage Clerk User with database
+
+const clerkWebhooks = async (requestAnimationFrame, res) => {
+  try {
+  } catch (error) {}
+};
