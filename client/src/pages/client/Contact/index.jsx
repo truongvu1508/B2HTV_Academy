40607073e3172ddfead697a37f86ea407e7e4dd4
truongvu1508 <ntv150804@gmail.com>
2025-04-24 22:05:55 +0700

    update Page Contact

--- client/src/pages/client/Contact/index.jsx
+++ client/src/pages/client/Contact/index.jsx
@@ -3,6 +3,8 @@ import Navbar from "../../../components/student/Navbar";
 import { Helmet } from "react-helmet";
 import Footer from "../../../components/student/Footer";
 import { assets } from "../../../assets/assets";
+import "./Contact.scss";
+import BackToTop from "../../../components/client/BackToTop";
 
 const Contact = () => {
   return (
@@ -44,11 +46,62 @@ const Contact = () => {
             <div class="text-2xl font-semibold text-dark-1">
               <h3>Gửi tin nhắn</h3>
             </div>
+            <form
+              className="contact-form flex flex-col gap-8 pt-16 lg:pt-10"
+              nh-form-contact="3H205VFW1K"
+              action="/contact/send-info"
+              method="POST"
+              autoComplete="off"
+              noValidate
+            >
+              <div className="flex flex-col sm:flex-row gap-6">
+                <div className="w-full sm:w-1/2">
+                  <label class="text-16 lh-1 fw-500 text-dark-1 mb-10">
+                    Họ tên *
+                  </label>
+                  <input
+                    type="text"
+                    name="full_name"
+                    className="form-control input-cus"
+                  />
+                </div>
+                <div className="w-full sm:w-1/2">
+                  <label class="text-16 lh-1 fw-500 text-dark-1 mb-10">
+                    Số điện thoại *
+                  </label>
+                  <input
+                    type="phone"
+                    name="phone"
+                    className="form-control input-cus"
+                  />
+                </div>
+              </div>
+              <div className="w-full">
+                <label class="text-16 lh-1 fw-500 text-dark-1 mb-10">
+                  Tiêu đề
+                </label>
+                <input
+                  type="text"
+                  name="full_name"
+                  className="form-control input-cus"
+                />
+              </div>
+              <div className="w-full">
+                <label class="text-16 lh-1 fw-500 text-dark-1 mb-10">
+                  Nội dung
+                </label>
+                <textarea name="content" id="" className="input-cus"></textarea>
+              </div>
+              <button className="button w-fit" type="submit">
+                Gửi tin nhắn
+              </button>
+            </form>
           </div>
         </div>
       </div>
 
       <Footer />
+      <BackToTop />
     </>
   );
 };
