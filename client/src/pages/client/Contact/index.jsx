c6b0d740267dbb7a547c68b3fc6ee7022c3a845c
truongvu1508 <ntv150804@gmail.com>
2025-04-24 22:14:33 +0700

    update page Contact

--- client/src/pages/client/Contact/index.jsx
+++ client/src/pages/client/Contact/index.jsx
@@ -15,10 +15,10 @@ const Contact = () => {
 
       <Navbar />
 
-      <div className="container mt-[80px] mb-[80px] h-screen">
-        <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
-          <div class="p-4 col-span-1">
-            <div class="text-2xl font-semibold text-dark-1">
+      <div className="container mt-[80px] mb-[80px]">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+          <div className="p-4 col-span-1">
+            <div className="text-2xl font-semibold text-dark-1">
               <h3>Liên lạc với chúng tôi</h3>
             </div>
             <div className="flex flex-col gap-5 mt-10">
@@ -42,8 +42,8 @@ const Contact = () => {
               </div>
             </div>
           </div>
-          <div class="p-4 col-span-2">
-            <div class="text-2xl font-semibold text-dark-1">
+          <div className="p-4 col-span-2">
+            <div className="text-2xl font-semibold text-dark-1">
               <h3>Gửi tin nhắn</h3>
             </div>
             <form
@@ -56,7 +56,7 @@ const Contact = () => {
             >
               <div className="flex flex-col sm:flex-row gap-6">
                 <div className="w-full sm:w-1/2">
-                  <label class="text-16 lh-1 fw-500 text-dark-1 mb-10">
+                  <label className="text-16 lh-1 fw-500 text-dark-1 mb-10">
                     Họ tên *
                   </label>
                   <input
@@ -66,7 +66,7 @@ const Contact = () => {
                   />
                 </div>
                 <div className="w-full sm:w-1/2">
-                  <label class="text-16 lh-1 fw-500 text-dark-1 mb-10">
+                  <label className="text-16 lh-1 fw-500 text-dark-1 mb-10">
                     Số điện thoại *
                   </label>
                   <input
@@ -77,7 +77,7 @@ const Contact = () => {
                 </div>
               </div>
               <div className="w-full">
-                <label class="text-16 lh-1 fw-500 text-dark-1 mb-10">
+                <label className="text-16 lh-1 fw-500 text-dark-1 mb-10">
                   Tiêu đề
                 </label>
                 <input
@@ -87,7 +87,7 @@ const Contact = () => {
                 />
               </div>
               <div className="w-full">
-                <label class="text-16 lh-1 fw-500 text-dark-1 mb-10">
+                <label className="text-16 lh-1 fw-500 text-dark-1 mb-10">
                   Nội dung
                 </label>
                 <textarea name="content" id="" className="input-cus"></textarea>
@@ -97,6 +97,16 @@ const Contact = () => {
               </button>
             </form>
           </div>
+        </div>
+        <div className="mt-10 w-full">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3833.771504114667!2d108.20855337836407!3d16.07734274490404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142184792140755%3A0xd4058cb259787dac!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBTxrAgUGjhuqFtIEvhu7kgdGh14bqtdCAtIMSQ4bqhaSBo4buNYyDEkMOgIE7hurVuZw!5e0!3m2!1svi!2s!4v1745507464518!5m2!1svi!2s"
+            className="w-full h-[450px]"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
         </div>
       </div>
 
