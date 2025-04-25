39ad84df509154b412ac8217fb4d88e1109204c8
truongvu1508 <ntv150804@gmail.com>
2025-04-25 23:34:00 +0700

    add educatorController

---
+++ server/controllers/educatorController.js
@@ -0,0 +1,17 @@
+import { clerkClient } from "@clerk/express";
+
+//Cập nhật role educator
+export const updateRoleToEducator = async () => {
+  try {
+    const userId = req.auth.userId;
+
+    await clerkClient.users.updateUserMetadata(userId, {
+      publicMetadata: {
+        role: "educator",
+      },
+    });
+    res.json({ success: true, message: "You can publish a course now" });
+  } catch (error) {
+    resizeBy.json({ success: false, message: error.message });
+  }
+};
