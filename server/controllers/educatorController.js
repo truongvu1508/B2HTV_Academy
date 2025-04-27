e3e6c6f1679ef5b5add331b094263b311714ec49
truongvu1508 <ntv150804@gmail.com>
2025-04-28 01:03:46 +0700

    add Controller Add New Course

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,5 +1,6 @@
 import { clerkClient } from "@clerk/express";
-
+import Course from "../models/Course";
+import { v2 as cloudinary } from "cloudinary";
 // Update role to educator
 export const updateRoleToEducator = async (req, res) => {
   try {
@@ -14,6 +15,32 @@ export const updateRoleToEducator = async (req, res) => {
       success: true,
       message: "Bạn có thể đăng khóa học ngay bây giờ.",
     });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Add New Course
+export const addCourse = async (req, res) => {
+  try {
+    const { courseData } = req.body;
+    const imageFile = req.file;
+    const educatorId = req.auth.userId;
+
+    if (!imageFile) {
+      return res.json({ success: false, message: "Ảnh không được đính kèm" });
+    }
+
+    const parsedCourseData = await JSON.parse(courseData);
+    parsedCourseData.educator = educatorId;
+
+    const newCourse = await Course.create(parsedCourseData);
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path);
+
+    newCourse.courseThumbnail = imageUpload.secure_url;
+
+    await newCourse.save();
+    res.json({ success: true, message: "Đã thêm khóa học" });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
