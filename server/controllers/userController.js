6f279f01b4978d2899d72d8fe5106410e489a74e
truongvu1508 <ntv150804@gmail.com>
2025-04-28 21:52:53 +0700

    Users Enrolled Courses With Lecture Links

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,7 +7,19 @@ export const getUserData = async (req, res) => {
     if (!user) {
       return res.json({ success: false, message: "Không tìm thấy người dùng" });
     }
-    req.json({ success: true, user });
+    res.json({ success: true, user });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Users Enrolled Courses With Lecture Links
+export const userEnrolledCourses = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const userData = await User.findById(userId).populate("enrolledCourses");
+
+    res.json({ success: true, enrolledCourses: userData.enrolledCourses });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
