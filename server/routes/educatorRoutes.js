04b1d183b7c971da917bf5c83a12edac3f25aba7
truongvu1508 <ntv150804@gmail.com>
2025-04-28 21:03:48 +0700

    update educatorRoutes

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   addCourse,
+  getEducatorCourses,
   updateRoleToEducator,
 } from "../controllers/educatorController.js";
 import upload from "../configs/multer.js";
@@ -16,5 +17,6 @@ educatorRouter.post(
   protectEducator,
   addCourse
 );
+educatorRouter.get("/courses", protectEducator, getEducatorCourses);
 
 export default educatorRouter;
