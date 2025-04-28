0cdaf496f114f3e57b9929b053503edb6fd847fd
truongvu1508 <ntv150804@gmail.com>
2025-04-28 21:39:39 +0700

    add CourseRoute

---
+++ server/routes/courseRoute.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { getAllCourse, getCourseId } from "../controllers/courseController.js";
+
+const courseRouter = express.Router();
+
+courseRouter.get("/all", getAllCourse);
+courseRouter.get("/:id", getCourseId);
