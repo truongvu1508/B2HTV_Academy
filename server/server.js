1c4c10bde1736497d545896c56cec843024f343c
truongvu1508 <ntv150804@gmail.com>
2025-04-29 20:42:38 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import educatorRouter from "./routes/educatorRoutes.js";
 import { clerkMiddleware } from "@clerk/express";
 import connectCloudinary from "./configs/cloudinary.js";
 import courseRouter from "./routes/courseRoute.js";
+import userRouter from "./routes/userRoutes.js";
 
 //Initialize Express
 const app = express();
@@ -24,6 +25,7 @@ app.get("/", (req, res) => res.send("API Working"));
 app.post("/clerk", express.json(), clerkWebhooks);
 app.use("/api/educator", express.json(), educatorRouter);
 app.use("/api/course", express.json(), courseRouter);
+app.use("/api/user", express.json(), userRouter);
 
 //Port
 const PORT = process.env.PORT || 5001;
