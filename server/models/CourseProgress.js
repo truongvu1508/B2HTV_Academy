efb0a31dab8891586e30f9b651facbdcaf2a022f
truongvu1508 <ntv150804@gmail.com>
2025-04-30 15:02:56 +0700

    add model CourseProgress

---
+++ server/models/CourseProgress.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const courseProgressSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    courseId: { type: String, required: true },
+    completed: { type: Boolean, required: true },
+    lectureCompleted: [],
+  },
+  { minimize: false }
+);
