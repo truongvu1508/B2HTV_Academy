9098da9eefb25974bf69d8d45977fd5bc99b005c
truongvu1508 <ntv150804@gmail.com>
2025-04-30 15:13:47 +0700

    get User Course Progress

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -111,3 +111,16 @@ export const updateUserCourseProgress = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// get User Course Progress
+export const getUserCourseProgress = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const { courseId } = req.body;
+    const progressData = await CourseProgress.findOne({ userId, courseId });
+
+    res.json({ success: true, progressData });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
