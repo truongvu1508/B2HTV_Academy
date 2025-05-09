d306c59934773508095daf3aff34a7a8e929f941
truongvu1508 <ntv150804@gmail.com>
2025-05-09 16:59:36 +0700

    update page Dashboard

--- client/src/components/student/CoursesSection/index.jsx
+++ client/src/components/student/CoursesSection/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { AppContext } from "../../../context/AppContext";
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
--- client/src/pages/educator/UpdateCourse/index.jsx
+++ client/src/pages/educator/UpdateCourse/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import uniqid from "uniqid";
