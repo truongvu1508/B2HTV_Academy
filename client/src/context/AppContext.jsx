9d536fd0c951eeb762398f032ea4be58c9758309
truongvu1508 <ntv150804@gmail.com>
2025-05-29 22:39:31 +0700

    update AppContext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
